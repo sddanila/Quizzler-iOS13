4923361c14e2df32cc050b5b81dfcb5f14698122
Danila Barton-Szabo <danilabarton-szabo@Enemy18s-MacBook-Pro.local>
2020-06-13 14:05:15 -0600

    add Question structure with data

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Danila Barton-Szabo on 2020-06-12.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,21 +16,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    @IBAction func answerButtonPressed(_ sender: UIButton) {
-    }
-    
     let quiz = [
-        "One plus one equals 3",
-        "Two plus 4 equals six",
-        "I am a dinosaur"
+        Question(text: "One plus one equals 3", answer: "True"),
+        Question(text: "Two plus 4 equals six", answer: "True"),
+        Question(text: "I am a dinosaur", answer: "False")
     ]
+    
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.questionLabel.text = "Is this a question?"
+        updateUI()
     }
-
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let currentAnswer = sender.currentTitle //True or False
+        if (quiz[questionNumber].answer == currentAnswer) {
+            print("Right")
+        } else {
+            print("Wrong")
+        }
+        
+        if (questionNumber != quiz.endIndex - 1) {
+            questionNumber += 1
+            updateUI()
+        } else {
+            print("Finished")
+            questionNumber = 0
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        self.questionLabel.text = quiz[questionNumber].text
+    }
 
 }
 
